5c75cec694175fd8818684b489ff2587edb202b4
Haolin Jiang <puibtx@gmail.com>
2022-10-31 19:14:43 -0700

    done V2

--- cal.py
+++ cal.py
@@ -1,41 +1,39 @@
 def calculate(s):
-    stack = []
-    s += '+'
-    pre_op = '+'
-    num = 0
-    i = 0
-    while i < len(s):
-        x = s[i]
-        if x.isdigit():
-            num = num * 10 + float(x)
-        if x in '+-*/':
-            if pre_op == '+':
-                stack.append(num)
-            if pre_op == '-':
-                stack.append(-num)
-            if pre_op == '*':
-                stack[-1] *= num
-            if pre_op == '/':
-                tmp = stack.pop()
-                stack.append(float(tmp/num))
-            num = 0
-            pre_op = x
-        if x == '(':
-            j = i
-            # use cnt to keep track of the entire ()
-            parenthesis = 0 
-            while j < len(s):
-                if s[j] == '(':
-                    parenthesis += 1 
-                if s[j] == ')':
-                    parenthesis -= 1 
-                if parenthesis == 0:
-                    break
-                j += 1
-            num = calculate(s[i+1:j+1])
-            i = j
-        i += 1
-    return sum(stack)
-    
-s = "(10+ 11)*(11/31)"
-print (calculate(s))
+    return eval(s)
+#def calculate(s):
+#     stack = []
+#     s += '+'
+#     pre_op = '+'
+#     num = 0
+#     i = 0
+#     while i < len(s):
+#         X = s[i]
+#         if X.isdigit():
+#             num = num * 10 + int(X)
+#         if X in '+-*/':
+#             if pre_op == '+':
+#                 stack.append(num)
+#             if pre_op == '-':
+#                 stack.append(-num)
+#             if pre_op == '*':
+#                 stack[-1] *= num
+#             if pre_op == '/':
+#                 tmp = stack.pop()
+#                 stack.append(int(tmp/num))
+#             num = 0
+#             pre_op = X
+#         if X == '(':
+#             j = i
+#             parent = 0 
+#             while j < len(s):
+#                 if s[j] == '(':
+#                     parent += 1 
+#                 if s[j] == ')':
+#                     parent -= 1 
+#                 if parent == 0:
+#                     break
+#                 j += 1
+#             num = calculate(s[i+1:j+1])
+#             i = j
+#         i += 1
+#     return sum(stack)
--- timed.py
+++ timed.py
@@ -1,8 +1,7 @@
 import time
 def timeme(func):
     def decorate(*args,**kwargs):
-    	start = time.time()
-    	time.sleep(2)
+    	start = time.time(
     	end = time.time()
     	print("Total time ",end - start)
 	return func(*args,**kwargs)
