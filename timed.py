ec66fa472e0054169aca9d7e405f843252f0f24a
puibtx <99428766+puibtx@users.noreply.github.com>
2022-11-02 22:15:24 -0700

    Update timed.py

--- timed.py
+++ timed.py
@@ -2,7 +2,7 @@ import time
 def timeme(func):
     def decorate(*args,**kwargs):
     	start = time.time()
-    	func(*args,**kwargs)
+	func(*args,**kwargs)
 	end = time.time()
     	print("Total time ",end - start)
     return decorate
